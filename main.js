df8107ee6c7aef990266f0874643b91db34279ce
scoble0002 <=>
2021-02-18 23:25:31 -0500

    added picks

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+
 
 //---------------random button---------------//
 
             
 function findMovie() {
-    let Movie = ["Citizen Kane", "The Apartment" ,"Fargo", "All ABout Eve", "Casablanca", "Double Indemity" , "A Face in the Crowd", "A Face in the Crowd"  ,"Do the Right Thing"
-                ,"The Treasure of the Sierra Madre","Boogie Nights","Rear Window","Pulp Fiction","Fast TImes at Ridgemont High","Boyz n the Hood","Midnight Cowboy",
-                "Sunset Bouleletd","Vertigo","Some Like it Hot","The Maltese Falcon","On the Waterfront","Touch of Evil",  ,"Cool Hand Luke","To Kill a Mockingbird",
-                "Who Framed Roger Rabbit","Duck Soup","Groundhog Day","The Asphalt Jungle","Wizard of Oz","This Spinal Tap","The Man Who Shot Liberty Valance","A Night at the Opera",
-                "Who's Afraid of Virginia Woolf","Dr Strangelove","Blazing Saddles","Night of the Hunter","The Thing","Unforgiven","The Big Lebowski","Raiders of the Lost Ark",
-                "Blade Runner","The Best Years of Our Lives","The Htch-Hiker","It Happened One Night","Sullivan's Travels","Singing in the Railroad","Psycho","Rocky","Young Frankenstein",
-                "Jaws","High Noon","Halloween","Ghostbusters","Shadow of the Doubt","Raising Arizonia","Big","Bonnie and Clyde","A Clockwork Orange","Stagecoach","The Royal Tennenbaums",
-                "Nation Lampoon's Vacation","The Wild Bunch","12 Angry Men","Alien","Coming to America" ,"Willy Wonka and The Chocalate Factory","Die Hard","No Country for Old Men",
-                "Dazed and Confused","The Searchers","Back to the Future","Planes Trains and Automobiles","LA Confidential","The Grapes of Wraith","Bringing up Baby","Scream","A Christmas Story",
-                "To Have or Have Not","The Shining","Ed Wood","Mr Smith Goes to Washington","Hail the Conquering Hero","First Blood","Dog Day Afternoon","The Terminator","E.T. The Extraterrestrial",
-                "Shaun of the Dead","The Lady Eve","North by Northwest","Apocalypse Now","Clueless","Taxi Driver","Platoon","What Ever Happened to Baby Jane","Get Out","Fight CLub",
-                "Gentlemen Prefer Blondes","Grease","Scott Pilgrim versus the World"
-];
-
+  if (question.value == "Superboy Prime") {
+    let Movie = ["Awesome!", ,];
     let random = Math.floor(Math.random() * (Movie.length - 1));
-
     document.getElementById("randomMovie").setAttribute("value", Movie[random]);
+  }
+  if (question.value != "Superboy Prime") {
+    let Movie = ["Wrong answer", "Weaklings like other weaklings", "Why do I even bother with you people?", "THAT GUY?", "REALLY?", "I don't even know who that is", "Didn't I beat him up?", ,];
+    let random = Math.floor(Math.random() * (Movie.length - 1));
+    document.getElementById("randomMovie").setAttribute("value", Movie[random]);
+}
 }
 
 
-
+//----------------sign up form-------------------//
 
 function formValidation() { 
         let name = document.forms["RegForm"]["Name"]; 
@@ -79,7 +79,7 @@ function formValidation() {
 
 
 
-  //---------------------pfi list sorter-------------------//
+  //---------------------div sorter-------------------//
 
   filterSelection("all")
   function filterSelection(c) {
@@ -115,6 +115,7 @@ function formValidation() {
   
   
   // Add active class to the current button (highlight it)
+
   let btnContainer = document.getElementById("myBtnContainer");
   let btns = btnContainer.getElementsByClassName("btn");
   for (let i = 0; i < btns.length; i++) {
